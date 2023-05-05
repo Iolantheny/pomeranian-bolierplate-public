a6796f95f6ea7aade2c24f7700ca13cf3af62f7f
Iolantheny <116835723+Iolantheny@users.noreply.github.com>
2023-05-05 19:29:56 +0200

    style

--- src/App/Exercises/HtmlCss/SecectorsAndCascade/index.jsx
+++ src/App/Exercises/HtmlCss/SecectorsAndCascade/index.jsx
@@ -1,5 +1,18 @@
 import './styles.css';
 
 export const SecectorsAndCascade = () => {
-  return <div>Change me</div>;
+  return (
+    <div className="container">
+      <div className="div1">Jestem bardzo unikalnym elementem</div>
+      <div className="div2">Jestem bardzo unikalnym elementem</div>
+      <div className="div3">
+        Jestem bardzo <p>unikalnym</p> elementem
+      </div>
+      <div className="div4">Jestem bardzo unikalnym elementem</div>
+      <div className="div5">
+        Jestem bardzo <p>unikalnym</p> elementem
+      </div>
+      <div className="div6">Jestem bardzo unikalnym elementem</div>
+    </div>
+  );
 };
