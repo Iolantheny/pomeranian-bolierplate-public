72fd19d1ec2601e7cdd265294ac631d3b01f9f2f
Iolantheny <116835723+Iolantheny@users.noreply.github.com>
2023-05-11 19:19:18 +0200

    table rating

---
+++ src/App/Exercises/HtmlCss/HTMLTables/data.js
@@ -0,0 +1,32 @@
+export const Data = [
+  {
+    id: 1,
+    location: 'Cape Verde Islands',
+    playerID: '#100120',
+    rating: 2,
+  },
+  {
+    id: 2,
+    location: 'Cape Verde Islands',
+    playerID: '#100122',
+    rating: 3,
+  },
+  {
+    id: 3,
+    location: 'Cape Verde Islands',
+    playerID: '#100124',
+    rating: 5,
+  },
+  {
+    id: 4,
+    location: 'United States of America',
+    playerID: '#100126',
+    rating: 1,
+  },
+  {
+    id: 5,
+    location: 'United States of America',
+    playerID: '#100128',
+    rating: 5,
+  },
+];
--- src/App/Exercises/HtmlCss/HTMLTables/index.jsx
+++ src/App/Exercises/HtmlCss/HTMLTables/index.jsx
@@ -1,59 +1,100 @@
 import './styles.css';
+import { Data } from './data';
+import { StarIcon } from '../../../Components/Icons/StarIcon';
 
 export const HTMLTables = () => {
   return (
-    <div className="tables">
-      <p>
-        1. The following data were collected by a group of students as they
-        tested four different types of plastics.
-      </p>
-      <table>
-        <tr>
-          <th>Plastic</th>
-          <th>Acetone</th>
-          <th>Flame test</th>
-          <th>Heat</th>
-          <th>Crease color</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>No effect</td>
-          <td>Green color</td>
-          <td>Softens</td>
-          <td>None</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Softended</td>
-          <td>No change</td>
-          <td>No change</td>
-          <td>White</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>No effect</td>
-          <td>Red color</td>
-          <td>Softens</td>
-          <td>None</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>No effect</td>
-          <td>Green color</td>
-          <td>Softens</td>
-          <td>None</td>
-        </tr>
+    <>
+      <div className="tables">
+        <p>
+          1. The following data were collected by a group of students as they
+          tested four different types of plastics.
+        </p>
+        <table>
+          <thead>
+            <tr>
+              <th>Plastic</th>
+              <th>Acetone</th>
+              <th>Flame test</th>
+              <th>Heat</th>
+              <th>Crease color</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>No effect</td>
+              <td>Green color</td>
+              <td>Softens</td>
+              <td>None</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>2</td>
+              <td>Softended</td>
+              <td>No change</td>
+              <td>No change</td>
+              <td>White</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>3</td>
+              <td>No effect</td>
+              <td>Red color</td>
+              <td>Softens</td>
+              <td>None</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>4</td>
+              <td>No effect</td>
+              <td>Green color</td>
+              <td>Softens</td>
+              <td>None</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>
+          Which plastic would be unsafe to use to store products that contain
+          acetone?
+        </p>
+        <ol>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ol>
+      </div>
+      <hr />
+      <table className="rating__table">
+        <thead>
+          <tr>
+            <th>Location</th>
+            <th>PLAYER_ID</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        {Data.map((row) => {
+          return (
+            <tbody key={row.id}>
+              <tr>
+                <td>{row.location}</td>
+                <td>{row.playerID}</td>
+                <td>
+                  {Array.from({ length: row.rating }, (_, i) => (
+                    <span key={i}>
+                      <StarIcon />
+                    </span>
+                  ))}
+                </td>
+              </tr>
+            </tbody>
+          );
+        })}
       </table>
-      <p>
-        Which plastic would be unsafe to use to store products that contain
-        acetone?
-      </p>
-      <ol>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ol>
-    </div>
+    </>
   );
 };
